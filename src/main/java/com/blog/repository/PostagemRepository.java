243c9f5b633fd43653dc488eff5342dc8f6a628a
Daniel Augusto <daniel.aug.souza@gmail.com>
2022-08-01 18:23:43 -0300

    Add findBy id/title; Add new post method

--- src/main/java/com/blog/repository/PostagemRepository.java
+++ src/main/java/com/blog/repository/PostagemRepository.java
@@ -1,13 +1,20 @@
 package com.blog.repository;
 
+import java.util.List;
+
 import com.blog.model.Postagem;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 /**
  * PostagemRepository
  */
+@Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+
+  public List <Postagem> findAllByTituloIgnoreCase(@Param("titulo") String titulo);
   
 }
 
