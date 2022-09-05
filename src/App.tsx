e4f4c3f3337d1f4ae56a68ace4e1c0e27ac5705e
Daniel Augusto <daniel.aug.souza@gmail.com>
2022-09-05 10:59:05 -0300

    Fix cadastro + Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,34 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import CadastroUsuario from './components/temas/listatema/ListaTema';
+import './App.css';
+import CadastroUsuario from './paginas/CadastroUsuario/CadastroUsuario';
 import Footer from './components/static/footer/Footer';
 import Home from './paginas/Home/Home';
 import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
-import ListaTema from './paginas/Login/Login';
+import ListaTema from './components/temas/listatema/ListaTema';
 import Login from './paginas/Login/Login';
 import Navbar from './components/static/navbar/Navbar';
-
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
+   <Router>
       <Navbar />
-
       <Routes>
-        <Route path="/home" element={ <Home /> } />
-        <Route path="/" element={ <Login /> } />
-        <Route path="/login" element={ <Login /> } />
-        <Route path="/cadastrousuario" element={ <CadastroUsuario /> } />
-        <Route path="/temas" element={ <ListaTema /> } />
-        <Route path="/posts" element={ <ListaPostagem /> } />
-      </Routes>
 
+        <Route path="/" element={<Login  />} />
+
+        <Route path="/home" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/cadastrousuario" element={<CadastroUsuario/>} />
+
+        <Route path="/temas" element={<ListaTema />} />
+
+        <Route path="/posts" element={<ListaPostagem />} />
+
+
+      </Routes>
       <Footer />
     </Router>
   );
--- src/paginas/Login/Login.tsx
+++ src/paginas/Login/Login.tsx
@@ -47,13 +47,13 @@ function Login() {
   }
 
   return (
-    <Grid container direction='row' justifyContent='center' alignItems='center'>
+      <Grid container direction='row' justifyContent='center' alignItems='center'>
       <Grid alignItems='center' xs={6}>
         <Box paddingX={20}>
           <form onSubmit={onSubmit}>
-            <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' style={{fontWeight: 'bold'}}>Entrar</Typography>
-            <TextField value={userLogin.usuario} onChange={( e:ChangeEvent<HTMLInputElement> ) => updatedModel(e)} id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
-            <TextField value={userLogin.senha} onChange={( e:ChangeEvent<HTMLInputElement> ) => updatedModel(e)} id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password'fullWidth />
+            <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Entrar</Typography>
+            <TextField value={userLogin.usuario} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
+            <TextField value={userLogin.senha} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password'fullWidth />
             <Box marginTop={2} textAlign='center'>
               <Button type='submit' variant='contained' color='primary'>
                 Logar
@@ -64,18 +64,16 @@ function Login() {
             <Box marginRight={1}>
               <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
             </Box>
-          <Link to="/cadastrousuario">
-            <Typography variant='subtitle1' gutterBottom align='center' style={{fontWeight: 'bold'}}>Cadastre-se</Typography>
-          </Link>
+            <Link to='/cadastrousuario'>
+              <Typography variant='subtitle1' gutterBottom align='center' className='textos1'>Cadastre-se</Typography>
+            </Link>
           </Box>
         </Box>
       </Grid>
       <Grid xs={6} style={{
-        backgroundImage: `url(https://i.imgur.com/d5bMdDJ.jpg)`,
-        backgroundRepeat: 'no-repeat', width: '100vh', minHeight: '100vh', backgroundSize: 'cover', backgroundPosition: 'center'
-      }}>
-
-      </Grid>
+          backgroundImage: `url(https://i.imgur.com/d5bMdDJ.jpg)`,
+          backgroundRepeat: 'no-repeat', width: '100vh', minHeight: '100vh', backgroundSize: 'cover', backgroundPosition: 'center'
+        }}></Grid>
     </Grid>
   );
 }
