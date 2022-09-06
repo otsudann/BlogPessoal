77f429c80a9a399a3aed53ebf2b7d60578532f4c
Daniel Augusto <daniel.aug.souza@gmail.com>
2022-09-06 20:46:17 -0300

    Upd Usuario fields

--- src/main/java/com/blog/model/Usuario.java
+++ src/main/java/com/blog/model/Usuario.java
@@ -1,31 +1,43 @@
 package com.blog.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Entity
 @Table(name = "usuarios")
 public class Usuario {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-    @NotBlank
-    private String nome;
-    @NotBlank
-    private String foto;
-    @NotBlank
-    @Schema(example = "email@email.com.br")
-    private String usuario;
-    @NotBlank
-    @Size(min = 8)
-    private String senha;
 
-    @OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
-    private List<Postagem> postagem;
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "O atributo Nome é Obrigatório!")
+	@Size(min = 3, max = 255)
+	private String nome;
+
+	@Size(max = 5000,
+	message = "O link da foto não pode ser maior do que 5000 caracteres")
+	private String foto;
+
+	@Schema(example = "email@email.com.br")
+	@NotNull(message = "O atributo Usuário é Obrigatório!")
+	@Email(message = "O atributo Usuário deve ser um email válido!")
+	private String usuario;
+
+	@NotBlank(message = "O atributo Senha é Obrigatório!")
+	@Size(min = 8, message = "A Senha deve ter no mínimo 8 caracteres")
+	private String senha;
+
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("usuario")
+	private List<Postagem> postagem;
 
     public Usuario(long id, String nome, String foto, String usuario, String senha) {
         this.id = id;
