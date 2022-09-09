57062919472bdf4fc81de38937777c1180e29f43
Daniel Augusto <daniel.aug.souza@gmail.com>
2022-09-09 08:53:41 -0300

    Upd style

--- src/components/static/footer/Footer.tsx
+++ src/components/static/footer/Footer.tsx
@@ -1,47 +1,36 @@
-import React from 'react';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import FacebookIcon from '@material-ui/icons/Facebook';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import Box from '@mui/material/Box';
 import {Typography, Grid } from '@material-ui/core';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
 
 import './Footer.css'
 
 function Footer(){
   return (
   <>
-  <Grid container direction="row" justifyContent="center" alignItems="center">
-
-    <Grid alignItems="center" item xs={12}>
-      <Box style={{ backgroundColor: "#3F51B5", height: "120px" }}>
-        <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-          <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
-        </Box>
-        <Box display="flex" alignItems="center" justifyContent="center">
-          <a href="https://www.facebook.com/generationbrasil" target="_blank">
-            <FacebookIcon style={{ fontSize: 60, color: "white" }} />
-          </a>
-          <a href="https://www.instagram.com/generationbrasil/" target="_blank">
-            <InstagramIcon style={{ fontSize: 60, color: "white" }} />
-          </a>
-          <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank">
-            <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
-          </a>
-        </Box>
-      </Box>
-      <Box style={{ backgroundColor: "#303F9F", height: "60px" }}>
-        <Box paddingTop={1}>
-          <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2020 Copyright:</Typography>
-        </Box>
-        <Box>
-          <a target="_blank" href="https://brasil.generation.org">
-            <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">brasil.generation.org</Typography>
-          </a>
-        </Box>
-      </Box>
-    </Grid>
-
-  </Grid>
+    <footer className="footer">
+      <section className="copyright">
+        <p>Copyright &copy; Generation Brasil</p>
+      </section>
+      <section className="social">
+        <p>[minhas redes]</p>
+        <a href="https://www.instagram.com/otsudann/" target="_blank">
+          <svg focusable="false" viewBox="0 0 24 24" aria-hidden="true" >
+            <path d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8 1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5 5 5 0 0 1-5 5 5 5 0 0 1-5-5 5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3 3 3 0 0 0 3 3 3 3 0 0 0 3-3 3 3 0 0 0-3-3z"></path>
+          </svg>
+        </a>
+        <a href="https://www.github.com/otsudann/" target="_blank">
+          <svg focusable="false" viewBox="0 0 24 24" aria-hidden="true" >
+            <path d="M12 1.27a11 11 0 00-3.48 21.46c.55.09.73-.28.73-.55v-1.84c-3.03.64-3.67-1.46-3.67-1.46-.55-1.29-1.28-1.65-1.28-1.65-.92-.65.1-.65.1-.65 1.1 0 1.73 1.1 1.73 1.1.92 1.65 2.57 1.2 3.21.92a2 2 0 01.64-1.47c-2.47-.27-5.04-1.19-5.04-5.5 0-1.1.46-2.1 1.2-2.84a3.76 3.76 0 010-2.93s.91-.28 3.11 1.1c1.8-.49 3.7-.49 5.5 0 2.1-1.38 3.02-1.1 3.02-1.1a3.76 3.76 0 010 2.93c.83.74 1.2 1.74 1.2 2.94 0 4.21-2.57 5.13-5.04 5.4.45.37.82.92.82 2.02v3.03c0 .27.1.64.73.55A11 11 0 0012 1.27"></path>
+          </svg>
+        </a>
+        <a href="https://www.linkedin.com/otsudann/" target="_blank">
+          <svg focusable="false" viewBox="0 0 24 24" aria-hidden="true" >
+            <path d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14m-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93h2.79M6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37h2.77z"></path>
+          </svg>          
+        </a>
+      </section>      
+    </footer>
   </>
  );
 }
--- src/components/static/navbar/Navbar.tsx
+++ src/components/static/navbar/Navbar.tsx
@@ -1,10 +1,4 @@
-import React from 'react';
-
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import {Link, useNavigate} from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 import useLocalStorage from 'react-use-localstorage';
 
 import './Navbar.css'
@@ -21,50 +15,18 @@ function Navbar(){
 
   return ( 
   <>
-    <AppBar position="static" className="bg-menu navbar">
-      <Toolbar variant="dense">
-        <Box style={{ cursor: "pointer" }} >
-          <Typography variant="h5" color="inherit" className="blog-name">
-            BlogPessoal
-          </Typography>
-        </Box>
-        <Box display="flex" justifyContent="start">
-          <Link to="/home" className="text-decorator-none" >
-            <Box mx={1} style={{ cursor: "pointer" }}>
-              <Typography variant="h6" color="inherit">
-                home
-              </Typography>
-            </Box>
-          </Link>
-          <Link to="/postagens" className="text-decorator-none" >
-            <Box mx={1} style={{ cursor: "pointer" }}>
-              <Typography variant="h6" color="inherit">
-                postagens
-              </Typography>
-            </Box>
-          </Link>
-          <Link to="/temas" className="text-decorator-none" >
-            <Box mx={1} style={{ cursor: "pointer" }}>
-              <Typography variant="h6" color="inherit">
-                temas
-              </Typography>
-            </Box>
-          </Link>
-          <Link to="/formularioTema" className="text-decorator-none" >
-            <Box mx={1} style={{ cursor: "pointer" }}>
-              <Typography variant="h6" color="inherit">
-                cadastrar tema
-              </Typography>
-            </Box>
-          </Link>
-          <Box mx={1} style={{ cursor: "pointer" }} onClick={goLogout}>
-            <Typography variant="h6" color="inherit">
-              temas
-            </Typography>
-          </Box>
-        </Box>
-        </Toolbar>
-      </AppBar>
+    <nav className="navbar">
+      <div className="list">
+        <ul>
+          <li><a href="/home">[otsublog home]</a></li>
+          <li><a href="/postagens">[postagens]</a></li>
+          <li><a href="/temas">[temas]</a></li>
+          <li><a href="/formularioTema">[novo tema]</a></li>
+          <li><a href="/formularioPostagem">[nova postagem]</a></li>
+          <li><a href="#" onClick={goLogout}>[sair]</a></li>
+        </ul>
+      </div>
+    </nav>
   </>
  )
 }
